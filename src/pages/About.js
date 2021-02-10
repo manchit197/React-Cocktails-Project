2eb68d04c6b1c293ba7b760fdae815f1d406a64c
Jeff Suen <manchit197@gmail.com>
2021-02-10 16:49:27 +0800

    initail commit

---
+++ src/pages/About.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum cupiditate similique deserunt repudiandae
+        dolorum tempora voluptates perspiciatis non obcaecati pariatur excepturi dolorem ratione, voluptatibus corporis
+        porro. Nihil, ut, eaque eligendi tempora dolore pariatur quos sequi ipsam quam odit quod. Odit quo recusandae
+        perspiciatis odio deserunt porro at quis nisi sapiente.
+      </p>
+    </section>
+  );
+};
+
+export default About;
